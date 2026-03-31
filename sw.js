c6cea4cb3ae03b74c0a9827ab8525e9be0729b1f
Vu Khuong An <vukhuongan@gmail.com>
2026-03-31 15:21:48 +0700

    Nuclear SW cache clear on first visit + force reload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== SERVICE WORKER — PTDTT Manager PWA =====
-const CACHE_NAME = 'ptdtt-v31032250';
+const CACHE_NAME = 'ptdtt-v31032260';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
