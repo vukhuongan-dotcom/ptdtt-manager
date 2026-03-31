2a00d577bcbb71eaede451e529d97acaf22c93f9
Vu Khuong An <vukhuongan@gmail.com>
2026-03-31 14:55:42 +0700

    Force clear old SW caches on load, aggressive cache busting

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== SERVICE WORKER — PTDTT Manager PWA =====
-const CACHE_NAME = 'ptdtt-v31032240';
+const CACHE_NAME = 'ptdtt-v31032250';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
