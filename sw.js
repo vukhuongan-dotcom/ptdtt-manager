e640dccb70cb544d25c7faa834a51d78f3837a89
Vu Khuong An <vukhuongan@gmail.com>
2026-03-31 12:42:04 +0700

    Bump SW cache v31032210

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== SERVICE WORKER — PTDTT Manager PWA =====
-const CACHE_NAME = 'ptdtt-v31032200';
+const CACHE_NAME = 'ptdtt-v31032210';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
