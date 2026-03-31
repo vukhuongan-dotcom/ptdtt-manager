cfc49fff75885adb0a191e3cccaab6e29e8be764
Vu Khuong An <vukhuongan@gmail.com>
2026-03-31 13:58:39 +0700

    Use local SheetJS instead of CDN, bump version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== SERVICE WORKER — PTDTT Manager PWA =====
-const CACHE_NAME = 'ptdtt-v31032230';
+const CACHE_NAME = 'ptdtt-v31032240';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
