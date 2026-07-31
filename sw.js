c4ab86157054d29c4648afdad7423829d65ec6ed
Vu Khuong An <vukhuongan@gmail.com>
2026-07-31 10:21:20 +0700

    feat(staff): cập nhật luân chuyển 9 BSNT chuyển đi từ 03/08 và kích hoạt 6 BSNT mới với mật khẩu ten123 từ 31/07

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== SERVICE WORKER — PTDTT Manager PWA =====
-const CACHE_NAME = 'ptdtt-v2607311017';
+const CACHE_NAME = 'ptdtt-v2607311021';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
