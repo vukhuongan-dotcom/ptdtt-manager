b85594a379b7c3ffc2af56e29eece72ac8c96495
Vu Khuong An <vukhuongan@gmail.com>
2026-07-31 11:05:19 +0700

    fix(rooms): cập nhật phòng B708 phân công BSNT Nguyễn Ngọc Minh Khôi đi chung với BSCKII Vũ Ngọc Anh Tuấn

--- js/rooms.js
+++ js/rooms.js
@@ -41,7 +41,7 @@ const ROOM_DATA = [
     { room: '719',  pod: 2, doctors: [{ id: 6,  role: 'chính' }, { id: 16, role: 'NT' }] },
 
     // POD 3 (Màu Xanh lá)
-    { room: '708',  pod: 3, doctors: [{ id: 4,  role: 'chính' }, { id: 44, role: 'NT' }] },
+    { room: '708',  pod: 3, doctors: [{ id: 4,  role: 'chính' }, { id: 46, role: 'NT' }] },
     { room: '709',  pod: 3, doctors: [{ id: 11, role: 'chính' }] },
     { room: '710',  pod: 3, doctors: [{ id: 7,  role: 'chính' }, { id: 45, role: 'NT' }] },
 ];
