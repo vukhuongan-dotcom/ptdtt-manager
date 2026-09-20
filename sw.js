49b84a654d7621e4a1a94e1549874323e266b8db
Vu Khuong An <vukhuongan@gmail.com>
2026-09-20 17:05:51 +0700

    feat(release): bump version to 2609201705 and update CSS bundle

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== SERVICE WORKER — PTDTT Manager PWA =====
-const CACHE_NAME = 'ptdtt-v2608252103';
+const CACHE_NAME = 'ptdtt-v2609201705';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
